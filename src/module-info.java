25fe2f29b85816f6439f4d8568471dcb06440540
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2023-02-25 23:54:38 -0300

    initial commit

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author gabriel
+ *
+ */
+module minesweeper {
+	requires org.junit.jupiter.api;
+	requires junit;
+}
---
+++ tests/io/github/eng_gabrielscardoso/JUnitTest.java
@@ -0,0 +1,16 @@
+package io.github.eng_gabrielscardoso;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnitTest {
+
+	@Test
+	void testIfEquals2() {
+		int a = 1 + 1;
+		
+		assertEquals(2, a);
+	}
+
+}
